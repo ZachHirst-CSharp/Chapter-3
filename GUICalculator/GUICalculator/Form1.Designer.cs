a5d96e5e6acb579b18497d0f4716dea4fc6a813b
Zach <zach_hirst@live.com>
2018-11-06 13:18:43 -0600

    Initial commit

---
+++ GUICalculator/GUICalculator/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace GUICalculator
+{
+    partial class LblReultAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbox1 = new System.Windows.Forms.TextBox();
+            this.txtbox2 = new System.Windows.Forms.TextBox();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.lblResultAdd = new System.Windows.Forms.Label();
+            this.BtnSub = new System.Windows.Forms.Button();
+            this.BtnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnMod = new System.Windows.Forms.Button();
+            this.lblResultSub = new System.Windows.Forms.Label();
+            this.lblResultMultiply = new System.Windows.Forms.Label();
+            this.lblResultDivide = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter A Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter another number";
+            // 
+            // txtbox1
+            // 
+            this.txtbox1.Location = new System.Drawing.Point(33, 36);
+            this.txtbox1.Name = "txtbox1";
+            this.txtbox1.Size = new System.Drawing.Size(100, 20);
+            this.txtbox1.TabIndex = 2;
+            // 
+            // txtbox2
+            // 
+            this.txtbox2.Location = new System.Drawing.Point(33, 122);
+            this.txtbox2.Name = "txtbox2";
+            this.txtbox2.Size = new System.Drawing.Size(100, 20);
+            this.txtbox2.TabIndex = 3;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(250, 9);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(75, 23);
+            this.BtnAdd.TabIndex = 4;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // lblResultAdd
+            // 
+            this.lblResultAdd.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultAdd.Location = new System.Drawing.Point(241, 60);
+            this.lblResultAdd.Name = "lblResultAdd";
+            this.lblResultAdd.Size = new System.Drawing.Size(100, 23);
+            this.lblResultAdd.TabIndex = 5;
+            this.lblResultAdd.Text = "Result";
+            this.lblResultAdd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblResultAdd.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // BtnSub
+            // 
+            this.BtnSub.Location = new System.Drawing.Point(396, 9);
+            this.BtnSub.Name = "BtnSub";
+            this.BtnSub.Size = new System.Drawing.Size(75, 23);
+            this.BtnSub.TabIndex = 6;
+            this.BtnSub.Text = "Subtract";
+            this.BtnSub.UseVisualStyleBackColor = true;
+            // 
+            // BtnMultiply
+            // 
+            this.BtnMultiply.Location = new System.Drawing.Point(250, 119);
+            this.BtnMultiply.Name = "BtnMultiply";
+            this.BtnMultiply.Size = new System.Drawing.Size(75, 23);
+            this.BtnMultiply.TabIndex = 7;
+            this.BtnMultiply.Text = "Multiply";
+            this.BtnMultiply.UseVisualStyleBackColor = true;
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(396, 120);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 23);
+            this.btnDivide.TabIndex = 8;
+            this.btnDivide.Text = "Divide";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            // 
+            // btnMod
+            // 
+            this.btnMod.Location = new System.Drawing.Point(325, 215);
+            this.btnMod.Name = "btnMod";
+            this.btnMod.Size = new System.Drawing.Size(75, 23);
+            this.btnMod.TabIndex = 9;
+            this.btnMod.Text = "Modulus";
+            this.btnMod.UseVisualStyleBackColor = true;
+            // 
+            // lblResultSub
+            // 
+            this.lblResultSub.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultSub.Location = new System.Drawing.Point(386, 60);
+            this.lblResultSub.Name = "lblResultSub";
+            this.lblResultSub.Size = new System.Drawing.Size(100, 23);
+            this.lblResultSub.TabIndex = 10;
+            this.lblResultSub.Text = "Result";
+            this.lblResultSub.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblResultMultiply
+            // 
+            this.lblResultMultiply.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultMultiply.Location = new System.Drawing.Point(241, 168);
+            this.lblResultMultiply.Name = "lblResultMultiply";
+            this.lblResultMultiply.Size = new System.Drawing.Size(100, 23);
+            this.lblResultMultiply.TabIndex = 11;
+            this.lblResultMultiply.Text = "Result";
+            this.lblResultMultiply.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblResultDivide
+            // 
+            this.lblResultDivide.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultDivide.Location = new System.Drawing.Point(386, 168);
+            this.lblResultDivide.Name = "lblResultDivide";
+            this.lblResultDivide.Size = new System.Drawing.Size(100, 23);
+            this.lblResultDivide.TabIndex = 12;
+            this.lblResultDivide.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(342, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // LblReultAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 325);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblResultDivide);
+            this.Controls.Add(this.lblResultMultiply);
+            this.Controls.Add(this.lblResultSub);
+            this.Controls.Add(this.btnMod);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.BtnMultiply);
+            this.Controls.Add(this.BtnSub);
+            this.Controls.Add(this.lblResultAdd);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.txtbox2);
+            this.Controls.Add(this.txtbox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LblReultAdd";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbox1;
+        private System.Windows.Forms.TextBox txtbox2;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Label lblResultAdd;
+        private System.Windows.Forms.Button BtnSub;
+        private System.Windows.Forms.Button BtnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnMod;
+        private System.Windows.Forms.Label lblResultSub;
+        private System.Windows.Forms.Label lblResultMultiply;
+        private System.Windows.Forms.Label lblResultDivide;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ GUICalculator/GUICalculator/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUICalculator
+{
+    public partial class LblReultAdd : Form
+    {
+        public LblReultAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            int num1;
+            int num2;
+            int sum;
+            num1 = Convert.ToInt32(txtbox1.Text);
+            num2 = Convert.ToInt32(txtbox2.Text);
+
+            sum = num1 + num2;
+            lblResultAdd.Text = "The sum is " + sum;
+        }
+
+       
+
+        private void BtnSub_Click(object sender, EventArgs e)
+        {
+            int num1;
+            int num2;
+            int difference;
+            num1 = Convert.ToInt32(txtbox1.Text);
+            num2 = Convert.ToInt32(txtbox2.Text);
+
+            difference = num1 - num2;
+            lblResultSub.Text = "The difference is " + difference;
+        }
+    }
+}
